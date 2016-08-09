82cf1c29f4d7c5fd119feb327b3ec13046954d34
Clément Renaud <clement.renaud@gmail.com>
2016-08-09 21:30:02 +0200

    node creation test

--- tests/Parser.test.js
+++ tests/Parser.test.js
@@ -30,6 +30,30 @@ describe('parser', () => {
       }, Error)
     })
 
+    describe('node creation', () => {
+
+      it('should allow node creation with options', ()=>{
+        let q = 'node add id:zappa color:blue name:"Frank Zappa" longitude:"1,23" latitude:"1,23" starred:false'
+
+        const { type, TopoQueryParser, selector, action, options } = parser.parse(q)
+        console.log(selector);
+
+        assert.deepEqual( selector, [{ id : null }] )
+        assert.equal(type, 'nodes')
+        assert.equal(q, TopoQueryParser)
+        assert.equal(action, 'ADD')
+        assert.deepEqual( options, [ {
+          color : 'blue',
+          name : 'Frank Zappa',
+          longitude: '1,23',
+          latitude: '1,23',
+          starred : false
+        } ])
+
+      })
+
+    })
+
     describe('link creation', () => {
 
       it('requires a target node as an option', ()=>{
--- tests/queries.js
+++ tests/queries.js
@@ -11,7 +11,8 @@ export default [
   'type:animal likes john',
   'name:"kitty" set type:cats',
   'cat set type:animals',
-  'cat set color:#FFFFFF longitude:1,23 latitude:1,23 starred:false'
+  'cat set color:#FFFFFF longitude:1,23 latitude:1,23 starred:false',
+  'node add color:blue name:"Frank Zappa" longitude:"1,23" latitude:"1,23" starred:false'
   // 'type:animal likes john hide',
   // 'node:cat edges',
   // 'node:type:animals edges'
